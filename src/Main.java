1ec993bac9398362b8ddc7a0c4aa302dd27e0b12
gyxx-xc <xdll_xc@outlook.com>
2023-08-26 12:12:56 +0800

    QR & some sort

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import javax.swing.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -12,33 +13,37 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        JFrame frame = new JFrame("test");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 1000);
-        frame.add(new QR());
-        frame.setVisible(true);
-        boolean wb = false;
+        QR localIPqr;
+        try {
+            localIPqr = new QR("http://" + getLocalHostExactAddress().getHostAddress()+"/");
+        } catch (Exception e) {
+            return;
+        }
+        localIPqr.showQRCode();
         try {
             ServerSocket serverSocket = new ServerSocket(80);
-            Path file = Paths.get(".", "src", "main.html");
+            Path file = Paths.get(".", "src", "html/main.html");
             String html;
             html = String.join("\n", Files.readAllLines(file));
+            html = html.replaceAll("localhost", getLocalHostExactAddress().getHostAddress());
             while (true) {
                 Socket client = serverSocket.accept();
                 OutputStream clientOutStream = client.getOutputStream();
                 InputStream in = client.getInputStream();
                 Scanner s = new Scanner(in, StandardCharsets.UTF_8);
                 String data = s.useDelimiter("\\r\\n\\r\\n").next();
-                System.out.println(data);
-                System.out.println();
-                Matcher get = Pattern.compile("^GET").matcher(data);
-                if (get.find()) {
+                Matcher get = Pattern.compile("^GET (.*) HTTP/").matcher(data);
+                if (get.find()) { // get command
+                    Matcher connectionM = Pattern.compile("Connection: (.*)").matcher(data);
+                    String[] connection = connectionM.group(1).split(", ");
+                    if (connection.length == 1 && connection[0].equals("keep-alive"));
                     Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
                     if (match.find()){
                         byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
@@ -56,19 +61,20 @@ public class Main {
                             int len = response[1] & 127;
                             byte[] mask = Arrays.copyOfRange(response, 2, 6);
                             for (int i = 0; i < len; i++) {
-                                in.read(response, 6 + i, 1);
-                                d[i] = (byte) (response[i + 6] ^ mask[i & 3] & 255);
+                                if (in.read(response, 6 + i, 1) != -1)
+                                    d[i] = (byte) (response[i + 6] ^ mask[i & 3] & 255);
+                                else
+                                    throw new RuntimeException("the data is not receive comprehensively");
                             }
-                            System.out.println(Arrays.toString(d));
-                            clientOutStream.write(response, 0, len+6);
-                            clientOutStream.flush();
+                            System.out.println(new String(d));
                         }
                     } else {
+                        localIPqr.close();
                         clientOutStream.write(
                                 ("""
                                         HTTP/1.1 200
                                         Content-Type: text/html
-                                                                        
+                                        
                                         """ + html).getBytes()
                         );
                     }
@@ -79,5 +85,40 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static InetAddress getLocalHostExactAddress() {
+        try {
+            InetAddress candidateAddress = null;
+
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface iface = networkInterfaces.nextElement();
+                // 该网卡接口下的ip会有多个，也需要一个个的遍历，找到自己所需要的
+                for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = inetAddrs.nextElement();
+                    // 排除loopback回环类型地址（不管是IPv4还是IPv6 只要是回环地址都会返回true）
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了 就是我们要找的
+                            // ~~~~~~~~~~~~~绝大部分情况下都会在此处返回你的ip地址值~~~~~~~~~~~~~
+                            return inetAddr;
+                        }
+
+                        // 若不是site-local地址 那就记录下该地址当作候选
+                        if (candidateAddress == null) {
+                            candidateAddress = inetAddr;
+                        }
+
+                    }
+                }
+            }
+
+            // 如果出去loopback回环地之外无其它地址了，那就回退到原始方案吧
+            return candidateAddress == null ? InetAddress.getLocalHost() : candidateAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/QR.java
+++ src/QR.java
@@ -1,10 +1,43 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
 
 public class QR extends JPanel {
+    public JFrame frame;
+    public final int SIZE;
+    private final BitMatrix bitMatrix;
+    public QR(String content) throws WriterException {
+        super();
+        bitMatrix = new QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, 0, 0);
+        SIZE = bitMatrix.getWidth();
+        frame = new JFrame("QRCode");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(150, 150, 100, 100);
+        for (int i = 0; i < SIZE; i ++) {
+            for (int j = 0; j < SIZE; j ++){
+                if (bitMatrix.get(i, j)) {
+                    g.fillRect(i*10, j*10, 10, 10);
+                }
+            }
+        }
+    }
+
+    public void showQRCode() {
+        frame.setSize((SIZE+10)*10, (SIZE+10)*10);
+        frame.add(this);
+        frame.setVisible(true);
+    }
+
+    public void close(){
+        frame.dispose();
     }
 }
