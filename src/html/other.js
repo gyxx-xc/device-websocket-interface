1d96dc9c823282373a88f367bd21eb0a3fbcaf2b
gyxx-xc <xdll_xc@outlook.com>
2023-08-27 14:44:42 +0800

    html style

--- src/html/other.js
+++ src/html/other.js
@@ -1,25 +1,22 @@
 html = document.querySelector('html');
-var a = false;
-  var StartEvt;
-  var moveEvt;
-  var endEvt;
-  if("ontouchstart" in window) {
-    document.addEventListener("touchstart", getTouchPos);
-    document.addEventListener("touchmove", getTouchPos);
-  } else {
-    document.addEventListener("mousedown", getMousePos);
-  }
 
+if("ontouchstart" in window) {
+  document.addEventListener("touchstart", getTouchPos);
+  document.addEventListener("touchmove", getTouchPos);
+} else {
+  document.addEventListener("mousedown", getMousePos);
+}
+
+var a = false;
 function getTouchPos(event) {
   var e = event || window.event;
   document.getElementById("q").textContent =
     (e.touches[0].pageY + ' ' + e.touches[0].pageX);
   send(e.touches[0].pageY + ' ' + e.touches[0].pageX);
     if (!a) {
-    html.requestFullscreen();
-    a = true;
+      html.requestFullscreen();
+      a = true;
     }
-
 }
 function getMousePos(event) {
   var e = event || window.event;
@@ -27,7 +24,7 @@ function getMousePos(event) {
     (e.pageY + ' ' + e.pageX);
   send(e.pageY + ' ' + e.pageX);
   if (!a) {
-  html.requestFullscreen();
-  a = true;
+    html.requestFullscreen();
+    a = true;
   }
 }
