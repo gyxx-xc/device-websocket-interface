333c92e2ad194109b3d80876dfe436fcbba00699
gyxx-xc <xdll_xc@outlook.com>
2023-08-27 14:42:32 +0800

    modularize the html

--- src/html/other.js
+++ src/html/other.js
@@ -1,6 +1,5 @@
 html = document.querySelector('html');
 var a = false;
-window.onload = function initControl() {
   var StartEvt;
   var moveEvt;
   var endEvt;
@@ -10,12 +9,12 @@ window.onload = function initControl() {
   } else {
     document.addEventListener("mousedown", getMousePos);
   }
-}
+
 function getTouchPos(event) {
   var e = event || window.event;
   document.getElementById("q").textContent =
     (e.touches[0].pageY + ' ' + e.touches[0].pageX);
-  ws.send(e.touches[0].pageY + ' ' + e.touches[0].pageX);
+  send(e.touches[0].pageY + ' ' + e.touches[0].pageX);
     if (!a) {
     html.requestFullscreen();
     a = true;
@@ -26,7 +25,7 @@ function getMousePos(event) {
   var e = event || window.event;
   document.getElementById("q").textContent =
     (e.pageY + ' ' + e.pageX);
-  ws.send(e.pageY + ' ' + e.pageX);
+  send(e.pageY + ' ' + e.pageX);
   if (!a) {
   html.requestFullscreen();
   a = true;
--- src/html/socket.js
+++ src/html/socket.js
@@ -1,11 +1,10 @@
 var ws;
-window.onload = function () {
-  if ("WebSocket" in window) {
-    ws = new WebSocket("ws://###HOST###"); // use ###HOST### as this computer
-  } else {
-    alert("WebSocket is not support!");
-  }
+if ("WebSocket" in window) {
+  ws = new WebSocket("ws://###HOST###"); // use ###HOST### as this computer
+} else {
+  alert("WebSocket is not support!");
 }
+
 function send(a) {
-ws.send(a)
+ws.send(a);
 }
